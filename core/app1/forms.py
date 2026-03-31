dc2e4376b94dee7c3d4a18491941fb77478bfb6a
Moueen-Togarvi <moueenuddintogarvi41@gmail.com>
2026-03-31 23:58:09 +0500

    update signup

--- core/app1/forms.py
+++ core/app1/forms.py
@@ -6,33 +6,19 @@ from .models import User
 
 
 class UserRegistrationForm(UserCreationForm):
-    """User registration form with additional fields"""
+    """Minimal User registration form"""
     email = forms.EmailField(required=True)
-    phone_number = forms.CharField(max_length=15, required=False)
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
-    profile_picture = forms.ImageField(required=False)
-    bio = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}), required=False)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'phone_number', 
-                 'profile_picture', 'bio', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
-            Row(
-                Column('first_name', css_class='form-group col-md-6 mb-0'),
-                Column('last_name', css_class='form-group col-md-6 mb-0'),
-                css_class='form-row'
-            ),
             'username',
             'email',
-            'phone_number',
-            'profile_picture',
-            'bio',
             Row(
                 Column('password1', css_class='form-group col-md-6 mb-0'),
                 Column('password2', css_class='form-group col-md-6 mb-0'),
@@ -43,11 +29,7 @@ class UserRegistrationForm(UserCreationForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        user.first_name = self.cleaned_data['first_name']
-        user.last_name = self.cleaned_data['last_name']
-        user.phone_number = self.cleaned_data['phone_number']
-        user.bio = self.cleaned_data['bio']
+        user.email = self.cleaned_data.get('email', '')
         if commit:
             user.save()
         return user
